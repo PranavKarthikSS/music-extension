803e6bf56600cf50fb348b42af187c468173a6ef
PranavKarthikSS <s.s.pranavkarthik1010@gmail.com>
2026-09-09 18:51:46 +0530

    added extensu=ion file and css file

---
+++ extension/extension.js
@@ -0,0 +1,9 @@
+export default class MusicPlayerExtension {
+    enable() {
+        console.log("Music Player extension enabled");
+    }
+
+    disable() {
+        console.log("Music Player extension disabled");
+    }
+}
