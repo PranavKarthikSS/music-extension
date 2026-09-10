a464e1a35df6ac5b04792f9ab4d33e583eb015a4
PranavKarthikSS <s.s.pranavkarthik1010@gmail.com>
2026-09-10 16:50:24 +0530

    Now thw music planel can identify the song details and the status of the song whether it is playing or in pause

--- extension/extension.js
+++ extension/extension.js
@@ -1,13 +1,21 @@
 import St from 'gi://St';
 import GObject from 'gi://GObject';
+import GLib from 'gi://GLib';
+
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 
+import { MprisPlayer } from './mpris/player.js';
+
+
 const MusicIndicator = GObject.registerClass(
 class MusicIndicator extends PanelMenu.Button {
+
     _init() {
         super._init(0.0, 'Music Player');
+
+        this._mpris = new MprisPlayer();
 
         this._icon = new St.Icon({
             icon_name: 'audio-x-generic-symbolic',
@@ -16,19 +24,89 @@ class MusicIndicator extends PanelMenu.Button {
 
         this.add_child(this._icon);
 
-        const titleItem = new PopupMenu.PopupMenuItem('Music Player');
-        titleItem.reactive = false;
+        this._titleItem = new PopupMenu.PopupMenuItem(
+            'Music Player'
+        );
 
-        const statusItem = new PopupMenu.PopupMenuItem('No music playing');
-        statusItem.reactive = false;
+        this._titleItem.reactive = false;
 
-        this.menu.addMenuItem(titleItem);
-        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-        this.menu.addMenuItem(statusItem);
+        this._statusItem = new PopupMenu.PopupMenuItem(
+            'No music playing'
+        );
+
+        this._statusItem.reactive = false;
+
+        this.menu.addMenuItem(this._titleItem);
+
+        this.menu.addMenuItem(
+            new PopupMenu.PopupSeparatorMenuItem()
+        );
+
+        this.menu.addMenuItem(this._statusItem);
+
+        this._updatePlayer();
+
+        this._updateTimer = GLib.timeout_add_seconds(
+            GLib.PRIORITY_DEFAULT,
+            2,
+            () => {
+                this._updatePlayer();
+                return GLib.SOURCE_CONTINUE;
+            }
+        );
+    }
+
+    _updatePlayer() {
+        try {
+            if (!this._mpris.findPlayer()) {
+                this._statusItem.label.text = 'No music player detected';
+                return;
+            }
+
+            const status =
+                this._mpris.getPlaybackStatus();
+
+            const metadata =
+                this._mpris.getMetadata();
+
+            if (!metadata) {
+                this._statusItem.label.text = 'No music playing';
+                return;
+            }
+		const title =
+   		 metadata['xesam:title'] || 'Unknown title';
+
+		const artists =
+    		metadata['xesam:artist'];
+
+		const artist =
+    		Array.isArray(artists)
+        	? artists.join(', ')
+        	: artists || 'Unknown artist';
+
+            this._statusItem.label.text =
+                `${title}\n${artist}\n\n${status}`;
+        } catch (error) {
+            logError(error, 'Music Player MPRIS error');
+        }
+    }
+
+    destroy() {
+        if (this._updateTimer) {
+            GLib.source_remove(this._updateTimer);
+            this._updateTimer = null;
+        }
+
+        this._mpris?.destroy();
+        this._mpris = null;
+
+        super.destroy();
     }
 });
 
+
 export default class MusicPlayerExtension {
+
     enable() {
         this._indicator = new MusicIndicator();
 
---
+++ extension/mpris/player.js
@@ -0,0 +1,102 @@
+import Gio from 'gi://Gio';
+import GLib from 'gi://GLib';
+
+export class MprisPlayer {
+    constructor() {
+        this._bus = Gio.bus_get_sync(
+            Gio.BusType.SESSION,
+            null
+        );
+
+        this._playerName = null;
+    }
+
+    findPlayer() {
+        const result = this._bus.call_sync(
+            'org.freedesktop.DBus',
+            '/org/freedesktop/DBus',
+            'org.freedesktop.DBus',
+            'ListNames',
+            null,
+            null,
+            Gio.DBusCallFlags.NONE,
+            -1,
+            null
+        );
+
+        const names = result.deep_unpack()[0];
+
+        for (const name of names) {
+            if (name.startsWith('org.mpris.MediaPlayer2.')) {
+                this._playerName = name;
+                break;
+            }
+        }
+
+        return this._playerName;
+    }
+
+    getProperty(interfaceName, propertyName) {
+        if (!this._playerName)
+            return null;
+
+        const result = this._bus.call_sync(
+            this._playerName,
+            '/org/mpris/MediaPlayer2',
+            'org.freedesktop.DBus.Properties',
+            'Get',
+            new GLib.Variant(
+                '(ss)',
+                [interfaceName, propertyName]
+            ),
+            null,
+            Gio.DBusCallFlags.NONE,
+            -1,
+            null
+        );
+
+        const variant = result.deep_unpack()[0];
+
+        return variant.deep_unpack();
+    }
+	getMetadata() {
+ 	   const metadata = this.getProperty(
+       	 'org.mpris.MediaPlayer2.Player',
+      	  'Metadata'
+   	 );
+
+   	 if (!metadata)
+      	  return null;
+
+  	  const result = {};
+
+  	  for (const [key, value] of Object.entries(metadata)) {
+       		 try {
+        	    result[key] = value.deep_unpack();
+      		  } catch (error) {
+           	 result[key] = value;
+       		 }
+   	 }
+
+   	 return result;
+	}
+
+    getPlaybackStatus() {
+        return this.getProperty(
+            'org.mpris.MediaPlayer2.Player',
+            'PlaybackStatus'
+        );
+    }
+
+    getPosition() {
+        return this.getProperty(
+            'org.mpris.MediaPlayer2.Player',
+            'Position'
+        );
+    }
+
+    destroy() {
+        this._bus = null;
+        this._playerName = null;
+    }
+}
